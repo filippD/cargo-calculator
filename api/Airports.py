79b983328401824fa2be60c0ddd7aa366a9d4376
filippD <philippdmitrienko@gmail.com>
2023-05-23 13:14:32 +0100

    Update Airports search service

--- api/Airports.py
+++ api/Airports.py
@@ -7,6 +7,12 @@ class Airports:
         result = []
         for key in airports:
           airport = airports[key]
-          if query in airport["iata"].lower()  or query in airport["icao"].lower() or query in airport["name"].lower():
+          if query in airport["iata"].lower():
             result.append(airport)
+
+        if len(result) < 10:
+          for key in airports:
+            airport = airports[key]
+            if query in airport["name"].lower() and airport not in result:
+              result.append(airport)
         return result[:10]
